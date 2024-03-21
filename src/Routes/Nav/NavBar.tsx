1565cc01e254e4dc82dc2f913b91c8ec145c858d
Tiffany Wang <thiffanyjun@hotmail.com>
2024-03-21 19:34:52 +1300

    add Navigation Bar

---
+++ src/Routes/Nav/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import "./NavBar.styles.scss"
+
+const NavBar = () => {
+  return (
+      <>
+          <div className="navigation">
+              <Link className="logo-container" to="/">
+                  <img
+                      className="logo"
+                      src="/images/logo.webp"
+                      alt="logo"
+                  />
+              </Link>
+
+              <div className="nav-links-container">
+                  <Link className="nav-link" to="/toys">
+                      TOYS
+                  </Link>
+                  <Link className="nav-link" to="/about">
+                      ABOUT
+                  </Link>
+              </div>
+          </div>
+          <Outlet />
+      </>
+  )
+}
+
+export default NavBar
