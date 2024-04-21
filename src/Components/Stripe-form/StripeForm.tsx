bdd126182821d1bd8e9734d96513365a5c206b43
Tiffany Wang <thiffanyjun@hotmail.com>
2024-04-22 11:58:02 +1200

    add payment handler

--- src/Components/Stripe-form/StripeForm.tsx
+++ src/Components/Stripe-form/StripeForm.tsx
@@ -7,58 +7,65 @@ import {
 } from '@stripe/react-stripe-js'
 import Button from '../Button/Button'
 import './StripeForm.styles.scss'
+import { useSelector } from 'react-redux'
+import { totalPrice } from '../../Store/Cart/cartSelector'
+import { selectedCurrentUser } from '../../Store/User/userSelector'
 
 const StripeForm = () => {
     const stripe = useStripe()
     const elements = useElements()
+    const amount = useSelector(totalPrice)
+    const currentUser = useSelector(selectedCurrentUser)
 
     const [errorMessage, setErrorMessage] = useState(null)
 
-    // const handleSubmit = async (event:any) => {
-    //     event.preventDefault()
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        if (!stripe || !elements) {
+            return
+        }
 
-    //     if (elements == null) {
-    //         return
-    //     }
+        const response = await fetch(
+            '/netlify/functions/create-payment-intent',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ amount: amount * 100 }),
+            }
+        ).then((res) => {
+            return res.json()
+        })
 
-    //     // Trigger form validation and wallet collection
-    //     const { error: submitError } = await elements.submit()
-    //     if (submitError) {
-    //         // Show error to your customer
-    //         setErrorMessage(submitError.message)
-    //         return
-    //     }
+        const clientSecret = response.paymentIntent.client_secret
+        const cardElement = elements.getElement(CardElement)
 
-    //     // Create the PaymentIntent and obtain clientSecret from your server endpoint
-    //     const res = await fetch('/create-intent', {
-    //         method: 'POST',
-    //     })
+        if (!cardElement) {
+            throw new Error('Card element not found')
+        }
 
-    //     const { client_secret: clientSecret } = await res.json()
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: cardElement,
+                billing_details: {
+                    name: currentUser?.displayName,
+                },
+            },
+        })
 
-    //     const { error } = await stripe.confirmPayment({
-    //         //`Elements` instance that was used to create the Payment Element
-    //         elements,
-    //         clientSecret,
-    //         confirmParams: {
-    //             return_url: 'https://example.com/order/123/complete',
-    //         },
-    //     })
+        if (paymentResult.error) {
+            alert(paymentResult.error.message)
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successful!')
+            }
+        }
+    }
 
-    //     if (error) {
-    //         // This point will only be reached if there is an immediate error when
-    //         // confirming the payment. Show error to your customer (for example, payment
-    //         // details incomplete)
-    //         setErrorMessage(error.message)
-    //     } else {
-    //         // Your customer will be redirected to your `return_url`. For some payment
-    //         // methods like iDEAL, your customer will be redirected to an intermediate
-    //         // site first to authorize the payment, then redirected to the `return_url`.
-    //     }
-    // }
     return (
         <div className="stripe-form-container">
-            <form className="stripe-form">
+            <form className="stripe-form" onSubmit={handleSubmit}>
                 <h2>Pay with Stripe</h2>
                 <CardElement className="stripe-card" />
                 <Button buttonType="inverted">Pay Now</Button>
@@ -68,3 +75,6 @@ const StripeForm = () => {
 }
 
 export default StripeForm
+function selectCurrentUser(state: unknown): unknown {
+    throw new Error('Function not implemented.')
+}
