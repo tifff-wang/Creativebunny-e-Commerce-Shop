58886859b9e20a46e4236426cb136e67a09a86b7
Tiffany Wang <thiffanyjun@hotmail.com>
2024-04-24 11:15:07 +1200

    update Nav bar

---
+++ src/Components/LandingPage/HeroSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <>
+     <div className="hero-container">
+     <div>
+      
+     </div>
+    </div>
+  </>
+   )
+}
+
+export default HeroSection
--- src/Routes/Nav/NavBar.tsx
+++ src/Routes/Nav/NavBar.tsx
@@ -7,6 +7,7 @@ import CartDropdown from '../../Components/Cart/CartDropdown'
 import { CartDropdownOpenStatus } from '../../Store/Cart/cartSelector'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectedCurrentUser, setCurrentUser } from '../../Store/User/userSlice'
+import Button from '../../Components/Button/Button'
 
 const NavBar = () => {
     const currentUser = useSelector(selectedCurrentUser)
@@ -32,19 +33,24 @@ const NavBar = () => {
 
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/toys">
-                        TOYS
+                        PRODUCTS
                     </Link>
-                    {currentUser ? (
-                        <p className="nav-link" onClick={handleClick}>
-                            LOGOUT
-                        </p>
-                    ) : (
-                        <Link className="nav-link" to="/auth">
-                            SIGN IN
-                        </Link>
-                    )}
-
                     <CartIcon />
+                    {currentUser ? (
+                        <Button
+                            buttonType='default'
+                            onClick={handleClick}
+                        >
+                            LOGOUT
+                        </Button>
+                    ) : (
+                        <Button
+                            buttonType='inverted'
+                            onClick={() => navigate('/auth')}
+                        >
+                            SIGN IN
+                        </Button>
+                    )}
                 </div>
                 {cartDropdownOpen && <CartDropdown />}
             </div>
