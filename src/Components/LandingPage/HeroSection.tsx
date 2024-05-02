7ec89aebff1a007389312ea48ecf881093c46837
Tiffany Wang <thiffanyjun@hotmail.com>
2024-05-03 11:00:01 +1200

    add navigation to hero section button

--- src/Components/LandingPage/HeroSection.tsx
+++ src/Components/LandingPage/HeroSection.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import Button from '../Button/Button'
-import "./HeroSection.styles.scss"
+import './HeroSection.styles.scss'
+import { useNavigate } from 'react-router-dom'
 
 const HeroSection = () => {
-  return (
-      <>
-          <div className="hero-container">
-              <div className="hero-image-container">
-                <img src="/images/toy-photos/bunny-shaker.webp" alt="a bunny shaker" className='hero-image'/>
-              </div>
-              <div className="hero-description-container">
-                  <h1 className='hero-title'>Premium Wooden Toys</h1>
-                  <h2 className='hero-subtilte'>Carefully handcrafted that provides fun and safey</h2>
-                  <Button buttonType="inverted">SHOP NOW</Button>
-              </div>
-          </div>
-      </>
-  )
+    const navigate = useNavigate()
+    return (
+        <>
+            <div className="hero-container">
+                <div className="hero-image-container">
+                    <img
+                        src="/images/toy-photos/bunny-shaker.webp"
+                        alt="a bunny shaker"
+                        className="hero-image"
+                    />
+                </div>
+                <div className="hero-description-container">
+                    <h1 className="hero-title">Premium Wooden Toys</h1>
+                    <h2 className="hero-subtilte">
+                        Carefully handcrafted that provides fun and safey
+                    </h2>
+                    <Button
+                        buttonType="inverted"
+                        onClick={() => navigate('/products')}
+                    >
+                        SHOP NOW
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default HeroSection
