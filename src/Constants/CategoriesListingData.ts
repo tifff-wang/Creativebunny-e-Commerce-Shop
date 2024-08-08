43f0c07d6613e8a59bcae5bf291e1899efbaf39d
Tiffany Wang <thiffanyjun@hotmail.com>
2024-08-08 16:48:48 +1200

    fix category background img links

--- src/Constants/CategoriesListingData.ts
+++ src/Constants/CategoriesListingData.ts
@@ -4,31 +4,31 @@ export const categoriesList: CategoryModel[] = [
     {
         id: 1,
         name: 'Pretend Play',
-        url: 'https://i.ibb.co/7Q2wVTT/dolltoy.webp',
+        url: '/images/popular-toys-photos/dolltoy.webp',
         route: 'products/pretend play',
     },
     {
         id: 2,
         name: 'Baby Shaker',
-        url: 'https://i.ibb.co/DYcY9f8/Screenshot-2024-03-27-at-10-44-44-AM.png',
+        url: '/images/popular-toys-photos/star-shaker.png',
         route: 'products/baby shakers',
     },
     {
         id: 3,
         name: 'Construction',
-        url: 'https://i.ibb.co/Cvcgy1v/mathtoy.webp',
+        url: '/images/popular-toys-photos/mathtoy.webp',
         route: 'products/construction',
     },
     {
         id: 4,
         name: 'Kitchen',
-        url: 'https://i.ibb.co/SK5d044/pastry.jpg',
+        url: '/images/popular-toys-photos/eggset.webp',
         route: 'products/kitchen',
     },
     {
         id: 5,
         name: 'Carpentry',
-        url: 'https://i.ibb.co/2vZ3b7P/toychair.webp',
+        url: '/images/popular-toys-photos/toychair.webp',
         route: 'products/pretend play',
     },
 ]
